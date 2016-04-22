d8cba3f37678a2919e4e18fa4e8a3b8b56ad595e
Yechiel K <chilikkal@gmail.com>
2016-04-21 21:24:49 -0400

    Update Igros.rb

--- Igros.rb
+++ Igros.rb
@@ -8,22 +8,22 @@ def igros
   
   if my_number3 >= 3
    puts "Are you sure? it sounds like your math is off"
-  
+   igros
    else 
     puts "The remainder for 3 was #{my_number3}. Now please divide your original number into 5 and submit the remainder."
     my_number5 = gets.strip.to_i
   end
   if my_number5 >= 5
    puts "Are you sure? it sounds like your math is off"
-  
+   igros
    else 
     puts "The remainder for 5 was #{my_number5}. Now please divide your original number into 7 and submit the remainder."
     my_number7 = gets.strip.to_i
   end
   if my_number7 >= 7
    puts "Are you sure? it sounds like your math is off"
-  
-   else 
+   igros
+  end 
     
     your_number = (my_number3 * 70) + (my_number5 * 21) + (my_number7 * 15)
       
@@ -32,7 +32,7 @@ def igros
     end
       
     
-    puts "You number was #{your_number}"
-  end
+    puts "Your number was #{your_number}"
+  
 end
 igros
